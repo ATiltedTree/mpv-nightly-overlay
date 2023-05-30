f38cc43921a54a770beb089901f62fd2ea39af7b
Tilmann Meyer <me@atiltedtree.dev>
2023-05-30 12:16:15 +0200

    Remove direct dependency on nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "MPV nightly flake";
 
   inputs = {
-    systems.url = "github:nix-systems/default";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     mpv-src = {
       url = "github:mpv-player/mpv";
       flake = false;
@@ -15,31 +13,25 @@
     
   };
 
-  outputs = { self, systems, nixpkgs, mpv-src, libplacebo-src }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = eachSystem (system: rec {
-        libplacebo = nixpkgs.legacyPackages.${system}.libplacebo.overrideAttrs(old: {
-          version = "9999";
+  outputs = { self, mpv-src, libplacebo-src }: let
+    packageFor = pkgs: (pkgs.mpv-unwrapped.override {
+      libplacebo = pkgs.libplacebo.overrideAttrs(old: {
+        version = "9999";
 
-          src = libplacebo-src;
-        });
-      
-        mpv-unwrapped = (nixpkgs.legacyPackages.${system}.mpv-unwrapped.override {inherit libplacebo;}).overrideAttrs(old: {
-          version = "9999";
-        
-          src = mpv-src;
-
-          patches = [
-            ./mpv-cycle.patch
-          ];
-        });
+        src = libplacebo-src;
       });
+    }).overrideAttrs(old: {
+      version = "9999";
 
-      overlays.default = final: prev: {
-        mpv-unwrapped = self.packages.${prev.system}.mpv-unwrapped;
-      };
+      src = mpv-src;
+
+      patches = [
+        ./mpv-cycle.patch
+      ];
+    });
+  in {
+    overlays.default = final: prev: {
+      mpv-unwrapped = packageFor prev;
     };
+  };
 }
